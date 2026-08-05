790ab5fcb9b14e22a62ed521ad44b95dae17e2b9
dev_young <rhsiddlsidhd@nate.com>
2026-08-06 08:28:23 +0900

    feat: Home page.tsx에 인기 상품 조회 배선
    - Promise.all에 getPopularProductsService(POPULAR_PRODUCTS_LIMIT).catch(() => []) 추가
    - HomeTemplate에 popularProducts 전달
    - userId 미전달(revalidate=3600 ISR 공유 캐시, isLiked 항상 false 유지)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,32 @@
 export const revalidate = 3600;
 
 import { HomeTemplate } from "./_components";
-import { getFeaturedTemplatesService, getProductService, Product } from "@/server/services";
+import {
+  getFeaturedTemplatesService,
+  getPopularProductsService,
+  getProductService,
+  Product,
+} from "@/server/services";
+import { POPULAR_PRODUCTS_LIMIT } from "@/shared/constants";
 
 const page = async () => {
   const previewProductId = process.env.NEXT_PUBLIC_MAIN_PREVIEW_PRODUCT_ID;
   const infoId = process.env.NEXT_PUBLIC_MAIN_PREVIEW_INFO_ID;
 
-  const [product, invitation] = await Promise.all([
+  const [product, invitation, popularProducts] = await Promise.all([
     previewProductId ? getProductService(previewProductId) : null,
     getFeaturedTemplatesService("invitation").catch(() => [] as Product[]),
+    getPopularProductsService(POPULAR_PRODUCTS_LIMIT).catch(() => [] as Product[]),
   ]);
 
-  return <HomeTemplate invitation={invitation} product={product} infoId={infoId} />;
+  return (
+    <HomeTemplate
+      invitation={invitation}
+      product={product}
+      infoId={infoId}
+      popularProducts={popularProducts}
+    />
+  );
 };
 
 export default page;
